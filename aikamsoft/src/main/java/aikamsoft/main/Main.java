68f39c8383f3a0112a699b51fd0133c445022592
kymaka <seafth345@gmail.com>
2022-08-30 21:25:38 +0700

    Some quality of code changes. Still mess for testing purposes.

--- aikamsoft/src/main/java/aikamsoft/main/Main.java
+++ aikamsoft/src/main/java/aikamsoft/main/Main.java
@@ -1,13 +1,26 @@
 package aikamsoft.main;
 
+import aikamsoft.parser.InputParser;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-  public static void main(String[] args) {
-    String operation = args[0];
-    String input_file = args[1];
-    String output_file = args[2];
-    System.out.println(operation);
-    System.out.println(input_file);
-    System.out.println(output_file);
+  public static void main(String[] args) throws FileNotFoundException {
+
+    String operation = null;
+    if (args.length == 3) {
+      operation = args[0];
+      String input_file = args[1];
+      String output_file = args[2];
+    }
+/*    if (operation.equals("search")) {
+      System.out.println(operation);
+    }
+    if (operation.equals("stat")) {
+      System.out.println(operation);
+    }*/
+
+    InputParser inputParser = new InputParser();
+    inputParser.parser("aikamsoft/src/main/resources/input.json");
   }
 }
