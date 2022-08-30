7aae0f5a3e5a61bad5ba49638e58bfc8a0127b83
kymaka <seafth345@gmail.com>
2022-08-30 19:20:55 +0700

    Added parsing arguments.

--- aikamsoft/src/main/java/aikamsoft/main/Main.java
+++ aikamsoft/src/main/java/aikamsoft/main/Main.java
@@ -3,6 +3,11 @@ package aikamsoft.main;
 public class Main {
 
   public static void main(String[] args) {
-
+    String operation = args[0];
+    String input_file = args[1];
+    String output_file = args[2];
+    System.out.println(operation);
+    System.out.println(input_file);
+    System.out.println(output_file);
   }
 }
