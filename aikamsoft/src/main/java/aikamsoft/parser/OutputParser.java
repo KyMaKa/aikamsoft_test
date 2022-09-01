596942df668aac9bae49f93a0dfa67ad0f93dc87
kymaka <seafth345@gmail.com>
2022-09-02 01:32:20 +0700

    Parses data to a file with given name.

--- aikamsoft/src/main/java/aikamsoft/parser/OutputParser.java
+++ aikamsoft/src/main/java/aikamsoft/parser/OutputParser.java
@@ -1,5 +1,19 @@
 package aikamsoft.parser;
 
+import aikamsoft.objects.entitys.Results;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class OutputParser {
+
+  public void write(String filename, Results results) throws IOException {
+
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    FileWriter writer = new FileWriter(filename);
+    gson.toJson(results, writer);
+    writer.close();
+  }
 
 }
