e0b5da270a3af2689e249479687cd5357ebe42c1
kymaka <seafth345@gmail.com>
2022-09-02 01:29:18 +0700

    Little tweaks.

--- aikamsoft/src/main/java/aikamsoft/parser/InputParser.java
+++ aikamsoft/src/main/java/aikamsoft/parser/InputParser.java
@@ -1,5 +1,6 @@
 package aikamsoft.parser;
 
+import aikamsoft.objects.criterias.Criteria;
 import aikamsoft.objects.criterias.Expenses;
 import aikamsoft.objects.criterias.LastName;
 import aikamsoft.objects.criterias.PassiveCustomers;
@@ -8,40 +9,35 @@ import aikamsoft.objects.types.Types;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class InputParser {
 
-  public void parser(String filename) throws FileNotFoundException {
+  public List<Criteria> parse(String filename) throws FileNotFoundException {
+
     FileReader reader = new FileReader(filename);
     Gson gson = new Gson();
     JsonElement jsonElement = gson.fromJson(reader, JsonElement.class);
     JsonArray jsonArray = jsonElement.getAsJsonObject().get("criterias").getAsJsonArray();
 
-    LastName lastName = null;
-    Expenses expenses = null;
-    PassiveCustomers passiveCustomers = null;
-    ProductName productName = null;
+    List<Criteria> criterias = new LinkedList<>();
+
     for (JsonElement j : jsonArray) {
       String type = j.getAsJsonObject().get("type").getAsString();
+
       if (type.equals(Types.lastName.name()))
-        lastName = gson.fromJson(j, LastName.class);
+        criterias.add(gson.fromJson(j, LastName.class));
       else if (type.equals(Types.product.name()))
-        productName = gson.fromJson(j, ProductName.class);
+        criterias.add(gson.fromJson(j, ProductName.class));
       else if (type.equals(Types.cost.name()))
-        expenses = gson.fromJson(j, Expenses.class);
+        criterias.add(gson.fromJson(j, Expenses.class));
       else if (type.equals(Types.passiveCustomers.name()))
-        passiveCustomers = gson.fromJson(j, PassiveCustomers.class);
+        criterias.add(gson.fromJson(j, PassiveCustomers.class));
     }
-    System.out.println(lastName != null ? lastName.getName() : null);
-
-    System.out.println(productName != null ? productName.getName() : null);
-
-    System.out.println(expenses != null ? expenses.getMaxExpenses() : null);
-
-    System.out.println(passiveCustomers != null ? passiveCustomers.getValue() : null);
+    return criterias;
   }
 
 }
